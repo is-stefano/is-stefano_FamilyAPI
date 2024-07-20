09e418ec5064bf0eb5b80f0861f1d9a22973bb76
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-07-20 11:10:16 +0000

    Feat: FirstCommit

--- src/app.py
+++ src/app.py
@@ -6,39 +6,60 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+jackson_family = FamilyStructure("Jackson")  # Create the jackson family object
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def handle_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = {"hello": "world",
+                    "family": members}
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/members<int:member_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_members(member_id):
+    response_body = {'id': '',
+                    'name': '',
+                    'age': '',
+                    'lucky_numbers': []}
+    if request.method == 'GET':
+        row = jackson_family.get_member(member_id)
+        print(row)
+        response_body['message'] = 'Usuario seleccionado'
+        response_body['results'] = member_id
+        return response_body, 200
+
+# @app.route('/members', methods=['POST'])
+# def handle_members():
+#     response_body = {'id': '',
+#                     'name': '',
+#                     'age': '',
+#                     'lucky_numbers': []}
+#     if request.method == 'POST':
+#         data = request.json
+#         members.append(data)
+#         response_body['message'] = 'Usuario agregado exitosamente'
+#         response_body['results'] = members
+#         return jsonify(response_body), 200
+
+# This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
